da065a583252335ca033e9ec4b407d1240b8b682
Fatima Ortega <fatimarortega@gmail.com>
2023-03-21 19:08:27 -0400

    input Text
    Learned how to input text and have it displayed as well

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { Planet } from "./Planet.js";
 
 function App() {
   const [age, setAge] = useState(0);
+  const [word, setWord] = useState("");
 
   const increaseAge = () => {
-   
     setAge(age + 1);
+  };
+
+  const displayInputValue = (event) => {
+    setWord(event.target.value);
   };
 
   return (
     <div className="App">
       {age}
       <button onClick={increaseAge}>Increase the age</button>
+      <div />
+      <input type="text" onChange={displayInputValue} />
+      {word}
       <Planet name="Jupiter" age={22} />
     </div>
   );
