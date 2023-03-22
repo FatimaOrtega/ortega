f6ecdcb276ceece297aa2e23482306ccad811c09
Fatima Ortega <fatimarortega@gmail.com>
2023-03-21 22:06:18 -0400

    Created an increase function
    Makes code easier to read

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(20);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="App">
       {count}
       <div />
       <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
+        onClick={increase}
       >
         Increase
       </button>
