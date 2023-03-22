791f271da4423c8a9fe1d7ec6014f2663b43c66a
Fatima Ortega <fatimarortega@gmail.com>
2023-03-21 20:43:55 -0400

    In line function
    A function that doesn't have any names and can still function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
 import { useState } from "react";
-import { Planet } from "./Planet.js";
 
 function App() {
-  const [age, setAge] = useState(0);
-  const [word, setWord] = useState("");
-
-  const increaseAge = () => {
-    setAge(age + 1);
-  };
-
-  const displayInputValue = (event) => {
-    setWord(event.target.value);
-  };
+  const [showText, setShowText] = useState(true);
 
   return (
     <div className="App">
-      {age}
-      <button onClick={increaseAge}>Increase the age</button>
-      <div />
-      <input type="text" onChange={displayInputValue} />
-      {word}
-      <Planet name="Jupiter" age={22} />
+      <button
+        onClick={() => {
+          alert("Hey");
+        }}
+      >
+        {" "}
+        Click to Alert{" "}
+      </button>
+      {showText && <h1>Hello</h1>}
     </div>
   );
 }
