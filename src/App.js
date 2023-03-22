9fc9f1337c0847302c5db34ce9c1dc4ed6f01f33
Fatima Ortega <fatimarortega@gmail.com>
2023-03-21 21:22:01 -0400

    Created a counter
    Implemented buttons that would increase, decrease, and set to zero on displayed value

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [showText, setShowText] = useState(true);
-  const [textColor, setTextColor] = useState("blue");
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
+      {count}
+      <div />
       <button
         onClick={() => {
-          setShowText(!showText);
+          setCount(count + 1);
         }}
       >
-        Click to Hide/Show
+        Increase
       </button>
-
+      <div />
       <button
         onClick={() => {
-          setTextColor(textColor === "blue" ? "red": "blue" );
+          setCount(count - 1);
         }}
       >
-        Change color
+        Decrease
       </button>
-      {showText && <h1 style={{color: textColor}}>Hello</h1>}
+      <div />
+      <button onClick={() => {
+        setCount(0);
+      }}>Set to 0</button>
     </div>
   );
 }
