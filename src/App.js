50a54916fcd3e2b7db770ff2c6369cbaa70df197
Fatima Ortega <fatimarortega@gmail.com>
2023-03-23 16:51:01 -0400

    deleteTask function works

--- src/App.js
+++ src/App.js
@@ -2,33 +2,44 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(20);
+  // creating a variable that will make a list of tasks
+  const [toDoList, setToDoList] = useState([]); //Creating an empty array
+  const [newTask, setnewTask] = useState("");
 
-  const increase = () => {
-    setCount(count + 1);
+  const handleChange = (event) => {
+    setnewTask(event.target.value);
   };
 
+  const addTask = () => {
+    //We can create a new variable or just add the code in setToDoList function
+    // const newTodoList = [...toDoList, newTask];
+    setToDoList([...toDoList, newTask]);
+  };
+
+  const deleteTask = (taskName) => {
+    const newTodoList = toDoList.filter((task) => task !== taskName);
+
+
+    setToDoList(newTodoList);
+  };
   return (
     <div className="App">
-      {count}
-      <div />
-      <button
-        onClick={increase}
-      >
-        Increase
-      </button>
-      <div />
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-      <div />
-      <button onClick={() => {
-        setCount(0);
-      }}>Set to 0</button>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className="list">
+        {toDoList.map((task) => {
+          return (
+            <div>
+              <h1>{task}</h1>
+              <button onClick={() => deleteTask(task)}>Delete Task</button>
+            </div>
+
+          );
+        })}
+      </div>
     </div>
   );
 }
