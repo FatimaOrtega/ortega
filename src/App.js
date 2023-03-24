b00df5a0d130caf14bab0a170a63a7581bc21962
Fatima Ortega <fatimarortega@gmail.com>
2023-03-23 20:35:02 -0400

    Implemented a navbar with a dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
 import "./App.css";
-import { useState } from "react";
-
-function App() {
-  // creating a variable that will make a list of tasks
-  const [toDoList, setToDoList] = useState([]); //Creating an empty array
-  const [newTask, setnewTask] = useState("");
-
-  const handleChange = (event) => {
-    setnewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    //We can create a new variable or just add the code in setToDoList function
-    // const newTodoList = [...toDoList, newTask];
-    setToDoList([...toDoList, newTask]);
-  };
-
-  const deleteTask = (taskName) => {
-    const newTodoList = toDoList.filter((task) => task !== taskName);
+import Navbar from "./components/Navbar";
 
 
-    setToDoList(newTodoList);
-  };
+const App = () => {
+  
   return (
+    <header>
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
+      <div className="nav-area">
+        <a href="#/" className="logo">A-Ortega</a>
+        <Navbar />
       </div>
-
-      <div className="list">
-        {toDoList.map((task) => {
-          return (
-            <div>
-              <h1>{task}</h1>
-              <button onClick={() => deleteTask(task)}>Delete Task</button>
-            </div>
-
-          );
-        })}
+      
       </div>
-    </div>
+      </header>
   );
 }
 
---
+++ src/menuItems.js
@@ -0,0 +1,21 @@
+export const menuItems = [
+    {
+        title: "Home",
+    }, {
+        title: "Services",
+        submenu: [
+            {
+                title: "Installation",
+            },
+            {
+                title: "Maintenance",
+            }
+        ],
+    },
+    {
+        title: "About Us",
+    },
+    {
+        title: "Contact Us",
+    },
+];
