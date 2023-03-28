186045faec400b65b89e3f52a2863147b3a0cd67
Fatima Ortega <fatimarortega@gmail.com>
2023-03-28 15:30:06 -0400

    Navigation bar component
    In order to keep the code clean, I made a separate component in order for the page to be cleaner

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
+import React from "react";
+import { NavigationBar } from "./components/navbar";
 
-import React from 'react';
-import Logo from "./components/assets/logo.svg"
-
-import { Container, Nav, Navbar, Row, Col, NavDropdown } from "react-bootstrap";
 import "./App.css";
-import { DropDownMenu } from "./components/Dropdownmenu";
-import { DropDownSubMenu } from "./components/dropdownsubmenu";
 
 const App = () => {
   return (
     <>
-      <Navbar bg="white" expand="lg" variant="light">
-        <Container>
-          <img src={Logo} href="/" className='logo'/>
-         
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <DropDownMenu title="Services" id="collabsible-nav-dropdown">
-              <DropDownSubMenu href="#action/3.7" title="Installation">
-                  <NavDropdown.Item href="#action/3.1">A/C</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Furnace</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Water Heating</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Ducts</NavDropdown.Item>
-                  </DropDownSubMenu>
-                <DropDownSubMenu href="#action/3.7" title="Maintenance">
-                <NavDropdown.Item href="#action/3.1">A/C</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Furnace</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Water Heating</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.1">Ducts</NavDropdown.Item>
-                </DropDownSubMenu>
-              </DropDownMenu>
-              <Nav.Link href="/">About Us</Nav.Link>
-              <Nav.Link href="/">Contact Us</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <NavigationBar />
     </>
   );
 };
---
+++ src/components/navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Logo from "./assets/logo.svg"
+import { Container, Nav, Navbar, Row, Col, NavDropdown } from "react-bootstrap";
+import { DropDownMenu } from "./Dropdownmenu";
+import { DropDownSubMenu } from "./dropdownsubmenu";
+
+export const NavigationBar = () => {
+    return (
+      <>
+    <Navbar bg="white" expand="lg" variant="light">
+        <Container>
+          <img src={Logo} href="/" className='logo'/>
+         
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <DropDownMenu title="Services" id="collabsible-nav-dropdown">
+              <DropDownSubMenu href="#action/3.7" title="Installation">
+                  <NavDropdown.Item href="#action/3.1">A/C</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Furnace</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Water Heating</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Ducts</NavDropdown.Item>
+                  </DropDownSubMenu>
+                <DropDownSubMenu href="#action/3.7" title="Maintenance">
+                <NavDropdown.Item href="#action/3.1">A/C</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Furnace</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Water Heating</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/9.1">Ducts</NavDropdown.Item>
+                </DropDownSubMenu>
+              </DropDownMenu>
+              <Nav.Link href="/">About Us</Nav.Link>
+              <Nav.Link href="/">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </>
+  )
+}
