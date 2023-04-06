b509e74a9aa3a7ca1b885f7afee0aff3ba7a1b4f
Fatima Ortega <fatimarortega@gmail.com>
2023-04-06 17:06:25 -0400

    Fixed up footer

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -4,7 +4,7 @@ export const AboutUs = () => {
     return (
     <div className="HeaderSection">
 
-       <h1> This is the About Us Page</h1>
+       <h1>About Us Page Is Coming Soon...</h1>
     </div>
     )
   
